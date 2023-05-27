7ee9f1322e639e7a418120e8887e917c5bd0e3a0
Avi Kaul <avikarkaul@gmail.com>
2023-05-27 15:48:59 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+//Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()+1);
+
+let num3 = 8;
+let num4 = 20;
+let mod = num4%num3;
+
+let set = [1,2,3,4,5,6,7,8,20];
+let max = Math.max(...set);
+
